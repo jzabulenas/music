1a1ad61cf47ed9ffaa3eeb8e1af20359f8b9264b
Julius <jzabulenas@protonmail.com>
2026-08-25 20:36:10 +0300

    Add controller for retrieving all blocked artists

--- backend/src/main/java/com/example/demo/blocked/BlockedArtistController.java
+++ backend/src/main/java/com/example/demo/blocked/BlockedArtistController.java
@@ -2,6 +2,7 @@ package com.example.demo.blocked;
 
 import com.example.demo.user.UserService;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +21,15 @@ class BlockedArtistController {
   ) {
     this.blockedArtistService = blockedArtistService;
     this.userService = userService;
+  }
+
+  @GetMapping
+  List<String> list(@AuthenticationPrincipal UserDetails userDetails) {
+    Long userId = this.userService.findByEmail(
+      userDetails.getUsername()
+    ).getId();
+
+    return this.blockedArtistService.getNames(userId);
   }
 
   @PostMapping
